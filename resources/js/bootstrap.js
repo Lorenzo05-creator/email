501bb658c14e63732f7aad8624399dd72c4b841f
Lorenzo05-creator <lory4jz@gmail.com>
2026-02-05 20:22:45 +0100

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,7 +3,4 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// --- Import Bootstrap CSS e JS ---
-import 'bootstrap/dist/css/bootstrap.min.css';  // CSS di Bootstrap
-import * as bootstrap from 'bootstrap';         // JS di Bootstrap
 
